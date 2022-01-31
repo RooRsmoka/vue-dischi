02f5edcfa03d0e2620153dfe249a78844af8e3c2
RooRsmoka <chri.disa96@gmail.com>
2022-01-31 15:45:10 +0100

    created components for the album display

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img :src="album.poster">
+        <h4>{{album.title}}</h4>
+        <p>{{album.author}}</p>
+        <p>{{album.year}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        album: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <album-card 
+        v-for="album in albums.response" 
+        :key="album.index"
+        :album="album"
+        />
+    </div>
+</template>
+
+<script>
+import AlbumCard from './AlbumCard.vue'
+export default {
+    components: {
+        AlbumCard
+    },
+    props: {
+        albums: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
