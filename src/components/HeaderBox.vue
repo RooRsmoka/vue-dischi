fbe4d734c83b73fffa9fe602b624e4c1fb075555
RooRsmoka <chri.disa96@gmail.com>
2022-02-02 20:35:14 +0100

    created header

---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="@/assets/spotify-logo.png">
+        </div>  
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+header {
+    background-color: $secondary-color;
+    height: 60px;
+    display: flex;
+
+    .logo {
+
+        img {
+            height: 100%;
+
+        }
+    }
+}
+</style>
