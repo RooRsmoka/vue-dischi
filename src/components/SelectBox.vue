a9903d81896488b88a6c871a3696626f432cf39b
RooRsmoka <chri.disa96@gmail.com>
2022-02-02 20:58:46 +0100

    created select

---
+++ src/components/SelectBox.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="select-box">
+        <select v-model="selectedGenre" @change="$emit('search', selectedGenre)">
+            <option value="all">All</option>
+            <option value="jazz">Jazz</option>
+            <option value="metal">Metal</option>
+            <option value="pop">Pop</option>
+            <option value="rock">Rock</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedGenre: 'all',
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+.select-box {
+    background-color: $primary-color;
+    display: flex;
+    justify-content: center;
+    padding: 15px 0;
+}
+</style>
